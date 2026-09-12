d20072d3fdd2aac240cb4833bc74e46738caf773
juanzz-111 <juanzitoplays15@gmail.com>
2026-09-12 13:34:02 +0000

    Adiciona rota para favicon e remove tipo de imagem desnecessário nos links de ícone

--- Meu-app/app.py
+++ Meu-app/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
+@app.route("/favicon.png")
+def favicon():
+    return send_from_directory("static", "favicon.png")
 
 @app.route("/")
 def inicio():
